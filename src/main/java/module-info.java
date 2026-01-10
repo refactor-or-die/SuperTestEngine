78ba6e44699c68f094c5a94d80a0db84e73a0dd8
StKucharski <stanislawkucharski205@gmail.com>
2026-01-10 15:04:43 +0100

    added a dependency of parser and used it in SaveNoMarkdown.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module superTestEngineFX {
     requires javafx.fxml;
     requires java.desktop;
     requires javafx.graphics;
+    requires org.commonmark;
 
     opens org.openjfx to javafx.fxml;
     exports org.openjfx;
